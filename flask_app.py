5a0c462edac947c476a6060b81798734d36e3e0d
Stefanos Aslanis <55686401+Allow-tap@users.noreply.github.com>
2021-10-11 13:05:05 +0200

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -10,18 +10,11 @@ app = Flask(__name__)
 def result_flask_app():
     result = tweet_analysis.delay()
     while result.ready() == False:
-<<<<<<< HEAD
         print("Executing..")
         time.sleep(8)
     data = result.get(timeout=1)
     return jsonify(data)
-    #return json.dumps(data, sort_keys=True)
-=======
-        print("Executing and gathering results, please wait, sleeping for 8 seconds...")
-        time.sleep(8)
-    data = result.get(timeout=1)
-    return json.dumps(data)
->>>>>>> 0703d70d6c8c168c29a9f379386b4ffbcc75926c
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=5000, debug=False)
